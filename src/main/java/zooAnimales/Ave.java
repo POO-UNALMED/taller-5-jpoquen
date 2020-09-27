36bf2253272c0f770ae36a0fd395ab2cbe262b0e
Juan Pablo <jpoukemy@gmail.com>
2020-09-27 18:18:12 -0500

    v15
    v15

--- src/main/java/zooAnimales/Ave.java
+++ src/main/java/zooAnimales/Ave.java
@@ -13,7 +13,7 @@ public class Ave extends Animal{
 	public static int aguilas;
 	private String colorPlumas;
 	
-	public Ave(){}
+	public Ave(){listado.add(this);}
 	
 	public Ave (String nombre, int edad, String habitat, String genero, String colorPlumas) {
 		super(nombre, edad, habitat, genero);
